bbece809aee01234cceeec919a528fa8023d0973
Seungjae Park <astro.psj@gmail.com>
2019-07-07 15:04:53 +0900

    Set conditional compile variable; server url

--- app/src/main/java/com/shim/user/shimapplication/retrofit/ServiceGenerator.java
+++ app/src/main/java/com/shim/user/shimapplication/retrofit/ServiceGenerator.java
@@ -1,12 +1,14 @@
 package com.shim.user.shimapplication.retrofit;
 
+import com.shim.user.shimapplication.BuildConfig;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
     public static ShimService create() {
         return new Retrofit.Builder()
-                .baseUrl("http://13.124.230.126/")
+                .baseUrl(BuildConfig.SERVER_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(ShimService.class);
