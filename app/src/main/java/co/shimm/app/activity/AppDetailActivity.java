f7b9b313eb90397a07fdf8f76ea83b5538c93d43
LieutenantKang <blizzard1107@gmail.com>
2019-07-24 01:15:53 +0900

    개인정보처리방침 추가

--- app/src/main/java/co/shimm/app/activity/AppDetailActivity.java
+++ app/src/main/java/co/shimm/app/activity/AppDetailActivity.java
@@ -1,9 +1,13 @@
 package co.shimm.app.activity;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import co.shimm.app.BuildConfig;
 import co.shimm.app.R;
@@ -11,12 +15,27 @@ import co.shimm.app.R;
 import static co.shimm.app.activity.MainActivity.isCurrentEtc;
 
 public class AppDetailActivity extends AppCompatActivity {
+    TextView versionText;
+    TextView privacy;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_detail);
-        TextView versionText = findViewById(R.id.text_app_version);
+        versionText = findViewById(R.id.text_app_version);
+        privacy = findViewById(R.id.text_app_privacy);
+
         versionText.setText("App Version : " + BuildConfig.VERSION_NAME);
+
+        Linkify.TransformFilter mTransform = new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher matcher, String s) {
+                return "";
+            }
+        };
+
+        Pattern pattern = Pattern.compile("개인정보처리방침");
+        Linkify.addLinks(privacy, pattern, "http://prography.org/privacy-policy", null,mTransform);
 
         isCurrentEtc = false;
     }
